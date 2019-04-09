1884dd25d6f43447004219a89c29bbef925b69e4
Carlos Costa <carlosdaviddacosta@gmail.com>
2019-04-09 14:54:10 +0200

    Update StarTopologyDataStructure.java

--- startopologydatastructure/StarTopologyDataStructure.java
+++ startopologydatastructure/StarTopologyDataStructure.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package startopologydatastructure;
 
 /**
- *
- * @author carlos
+ * @author carlos da costa
+ * @github github.com/calolocosta
  */
 import java.util.ArrayList;
 import java.util.Scanner;
